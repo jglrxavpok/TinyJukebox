960b51df187f0867e93c34cb2e3e29cd3c88f92d
jglrxavpok <jglrxavpok@gmail.com>
2019-06-11 23:14:44 +0200

    Fixed gitignore so that Session.kt can be pushed

---
+++ src/main/kotlin/org/jglrxavpok/tinyjukebox/auth/Session.kt
@@ -0,0 +1,80 @@
+package org.jglrxavpok.tinyjukebox.auth
+
+import org.jglrxavpok.tinyjukebox.Config
+import org.jglrxavpok.tinyjukebox.Timings
+import org.jglrxavpok.tinyjukebox.exceptions.InvalidCredentialsException
+import org.jglrxavpok.tinyjukebox.exceptions.InvalidSessionException
+import java.io.BufferedReader
+import java.io.PrintWriter
+import java.util.*
+
+class Session(val id: UUID, val username: String, val expirementDate: Long) {
+
+    companion object {
+        val Anonymous = Session(UUID.fromString("00000000-0000-0000-0000-000000000000"), "", Long.MAX_VALUE)
+
+        private val idMap = HashMap<String, Session>()
+
+        fun load(sessionId: String): Session {
+            // TODO: hashing?
+            val session = idMap[sessionId] ?: throw InvalidSessionException(sessionId)
+            if(session.isExpired()) {
+                close(sessionId)
+                throw InvalidSessionException(sessionId)
+            }
+
+            return session
+        }
+
+        fun open(username: String, passwordHash: String): Session {
+            if(AuthChecker.checkAuth(username, passwordHash)) {
+                // remove any existing session for the given username
+                idMap.filter { it.value.username == username }.forEach { idMap.remove(it.key) }
+
+                val sessionID = generateSessionID()
+                val session = Session(sessionID, username, System.currentTimeMillis()+Config[Timings.sessionExpiration])
+                idMap[sessionID.toString()] = session
+                return session
+            } else {
+                throw InvalidCredentialsException()
+            }
+        }
+
+        fun close(sessionId: String) {
+            idMap.remove(sessionId)
+        }
+
+        private fun generateSessionID(): UUID {
+            return UUID.randomUUID()
+        }
+
+        fun login(writer: PrintWriter, clientReader: BufferedReader, username: String, passwordHash: String) {
+            try {
+                val session = Session.open(username, passwordHash)
+                writer.println(session.id.toString())
+            } catch (e: InvalidCredentialsException) {
+                e.printStackTrace()
+                writer.println("no")
+            }
+        }
+
+        fun logout(writer: PrintWriter, clientReader: BufferedReader, username: String, passwordHash: String) {
+            Session.close(clientReader.readLine())
+        }
+
+        fun exists(sessionId: String): Boolean {
+            if(sessionId in idMap) {
+                val session = idMap[sessionId]!!
+                if(session.isExpired())
+                    return false
+                return session != Anonymous
+            }
+            return false
+        }
+    }
+
+    private fun isExpired(): Boolean {
+        return System.currentTimeMillis() >= expirementDate
+    }
+
+}

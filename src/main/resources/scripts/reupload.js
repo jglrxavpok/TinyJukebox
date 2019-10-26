8f2bb62ff1fd5b7302b51dc7ce91e217b47046ab
jglrxavpok <jglrxavpok@gmail.com>
2019-10-26 15:38:12 +0200

    Hack to make sure reupload button works in music pages

--- src/main/resources/scripts/reupload.js
+++ src/main/resources/scripts/reupload.js
@@ -1,16 +1,18 @@
 define(["jquery"], function($) {
     $(function() {
-        $(".playLink").each(function() {
-            var link = $(this);
-            link.click(function() {
-                var musicName = link.data("musicname");
+        setInterval(function() {
+            $(".playLink").each(function() {
+                var link = $(this);
+                link.off('click').click(function() {
+                    var musicName = link.data("musicname");
 
-                var xhttp = new XMLHttpRequest();
-                xhttp.open("POST", "/play/"+musicName, true);
-                xhttp.setRequestHeader("Content-Type", "application/octet-stream");
-                xhttp.setRequestHeader("File-Source", "Youtube");
-                xhttp.send("\n");
+                    var xhttp = new XMLHttpRequest();
+                    xhttp.open("POST", "/play/"+musicName, true);
+                    xhttp.setRequestHeader("Content-Type", "application/octet-stream");
+                    xhttp.setRequestHeader("File-Source", "Youtube");
+                    xhttp.send("\n");
+                });
             });
-        });
+        }, 1000);
     });
 });
